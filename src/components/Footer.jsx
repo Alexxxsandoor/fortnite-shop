ad60477d9dc869dd939c2d4effb67667abf30b77
Alexxxsandoor <v.verstukov14@gmail.com>
2022-07-11 03:15:25 +0300

    link repo header and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
 			<div className="row">
 				<p className="col s6 center">© {new Date().getFullYear()} Copyright Text</p>
 				<ul className="col s6 center">
-					<li><a className="grey-text text-lighten-3" href="#!">Repo</a></li>
+					<li><a className="grey-text text-lighten-3" href="https://github.com/Alexxxsandoor/fortnite-shop" target="_blank" rel="noreferrer">Repo</a></li>
 				</ul>
 				<a className="col s12 center grey-text text-lighten-3" href="https://t.me/alexxxsandoor">ALEXXXSANDOOR</a>
 			</div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ function Header() {
 	return (
 		<nav className=" blue-grey darken-1">
 			<div className="nav-wrapper">
-				<a href="!#" className="brand-logo">Shop</a>
+				<a href="/" className="brand-logo">Shop</a>
 				<ul id="nav-mobile" className="right hide-on-med-and-down">
-					<li><a href="https://github.com/Alexxxsandoor/fortnite-shop">Repo</a></li>
+					<li><a href="https://github.com/Alexxxsandoor/fortnite-shop" target="_blank" rel="noreferrer">Repo</a></li>
 				</ul>
 			</div>
 		</nav>
